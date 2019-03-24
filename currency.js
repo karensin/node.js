e25e897df07d7f9b03e9640eb9fb8de2ddeedb49
Karen k  sin <karenksin@Karens-Air.attlocal.net>
2019-03-23 22:37:22 -0700

    basic node app

---
+++ currency.js
@@ -0,0 +1,9 @@
+
+
+exports.currencyEUR= function(){
+  console.log('1 EUR = 1.1329401144 USD')
+}
+
+exports.currencyINR= function(){
+  console.log('0.0145712239 USD = 1 INR')
+}
---
+++ main.js
@@ -0,0 +1,16 @@
+const greetings = require("./greeting.js");
+const currency = require("./currency")
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+ greetings.greetingIcelandic();
+
+
+currency.currencyINR();
+
+currency.currencyEUR();
